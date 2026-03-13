ec08d421213b50732696506184a8373867e73beb
hamishg123 <hamishgordon051@gmail.com>
2026-03-13 12:34:40 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+// app.js
+
 const firebaseConfig = {
 apiKey: "AIzaSyAaSH_-zEHnM_xdUaxc2UkuFuEU5K-qu-0",
 authDomain: "siteguardian-a4905.firebaseapp.com",
@@ -6,76 +9,30 @@ projectId: "siteguardian-a4905"
 
 firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+const auth=firebase.auth();
+const db=firebase.firestore();
 
-const loginBtn = document.getElementById("loginBtn");
-const dashboard = document.getElementById("dashboard");
-const pricing = document.getElementById("pricing");
+const loginBtn=document.getElementById("loginBtn");
+const dashboard=document.getElementById("dashboard");
 
-loginBtn.onclick = () => {
+loginBtn.onclick=()=>{
 
-const provider = new firebase.auth.GoogleAuthProvider();
-
+const provider=new firebase.auth.GoogleAuthProvider();
 auth.signInWithPopup(provider);
 
 };
 
-auth.onAuthStateChanged(async user => {
+auth.onAuthStateChanged(async user=>{
 
 if(user){
 
-loginBtn.style.display="none";
-dashboard.classList.remove("hidden");
+loginBtn.innerText=user.displayName;
 
-const doc = await db.collection("users").doc(user.uid).get();
+dashboard.style.display="block";
+
+const ref=db.collection("users").doc(user.uid);
+const doc=await ref.get();
 
 if(!doc.exists){
-
-db.collection("users").doc(user.uid).set({
-tests:0
-});
-
-}
-
-}
-
-});
-
-document.getElementById("testBtn").onclick = async () => {
-
-const url = document.getElementById("urlInput").value;
-
-const user = auth.currentUser;
-
-const userRef = db.collection("users").doc(user.uid);
-
-const data = (await userRef.get()).data();
-
-if(data.tests >= 1){
-
-pricing.classList.remove("hidden");
-return;
-
-}
-
-document.getElementById("result").innerText = "Testing...";
-
-await fetch("http://YOUR_PI_IP:5000/test",{
-
-method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
-
-body:JSON.stringify({url:url})
-
-});
-
-userRef.update({
-tests:data.tests + 1
-});
-
-document.getElementById("result").innerText="Test sent. Results will appear soon.";
-
+await ref.set({tests:0});
 };
