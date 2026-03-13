31a70b2085ebcb20fd7888b361ea0606dbf688d1
hamishg123 <hamishgordon051@gmail.com>
2026-03-13 18:48:17 +0000

    Update app.js

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ const firebaseConfig = {
 
 
 
-const firebaseConfig = {
-apiKey:"YOUR_API_KEY",
-authDomain:"YOUR_PROJECT.firebaseapp.com",
-projectId:"YOUR_PROJECT"
-};
-
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
